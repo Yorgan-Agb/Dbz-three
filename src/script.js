7d0a7c7258aa05784af542abe4bf431ebf30fbce
agblemagnon.yorgan@gmail.com <agblemagnon.yorgan@gmail.com>
2022-02-01 20:16:42 +0100

    End first step

--- src/script.js
+++ src/script.js
@@ -5,29 +5,34 @@ import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
 import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import * as dat from 'lil-gui'
 
-/**
- * Base
- */
-// Debug
+
 const gui = new dat.GUI()
 // gui.add(SphereGeometry)
 
-// Canvas
+
 const canvas = document.querySelector('canvas.webgl')
 
-// Scene
+
 const scene = new THREE.Scene()
 const image = new Image()
 const texture = new THREE.Texture(image)
 image.addEventListener('load', () => {
     texture.needsUpdate = true
 })
-image.src = '/textures/matcaps/1étoile.png'
+image.src = '/textures/matcaps/Red.jpg'
 const loadingManager = new THREE.LoadingManager()
 console.log(loadingManager)
 const textureLoader = new THREE.TextureLoader(loadingManager)
-const crystalPower2 = textureLoader.load('/textures/matcaps/2étoile.png')
-const crystalPower3 = textureLoader.load('/textures/matcaps/2étoiles.png')
+const yellowFont = textureLoader.load('/textures/matcaps/Yellow.jpg')
+const redFont = textureLoader.load('/textures/matcaps/Red.jpg')
+const crystalPower1 = textureLoader.load('/textures/matcaps/1star.png')
+const crystalPower2 = textureLoader.load('/textures/matcaps/2stars.png')
+const crystalPower3 = textureLoader.load('/textures/matcaps/3stars.png')
+const crystalPower4 = textureLoader.load('/textures/matcaps/4stars.png')
+const crystalPower5 = textureLoader.load('/textures/matcaps/5stars.png')
+const crystalPower6 = textureLoader.load('/textures/matcaps/6stars.png')
+const crystalPower7 = textureLoader.load('/textures/matcaps/7stars.png')
+
 
 const fontLoader = new FontLoader()
 
@@ -47,37 +52,15 @@ fontLoader.load(
                 bevelSegments: 1
             }
         )
-        const textMaterial = new THREE.MeshBasicMaterial({color: 'yellow'})
+        const textMaterial = new THREE.MeshBasicMaterial({map:yellowFont})
         textGeometry.center()
+       
         const text = new THREE.Mesh(textGeometry, textMaterial)
         text.position.x = -1.5
         scene.add(text)
 
-        
-        { 
-        const sphereGeometry1 = new THREE.SphereGeometry(0.3,32,16)
-        const sphereMaterial1 = new THREE.MeshMatcapMaterial({map: crystalPower2})
-        const sphere1 = new THREE.Mesh(sphereGeometry1, sphereMaterial1)
-        sphere1.position.y = 1.5 
-        sphere1.position.z
-        sphere1.rotation.y = 4.5
-        
-        scene.add(sphere1)
-        const sphereGeometry2 = new THREE.SphereGeometry(0.3,32,16)
-        const sphereMaterial2 = new THREE.MeshMatcapMaterial({map: crystalPower2})
-        
-        const sphere2 = new THREE.Mesh(sphereGeometry2, sphereMaterial2)
-        sphere2.position.x = 2
-        sphere2.position.y = 2
-        sphere2.rotation.y = 4
-        
-        scene.add(sphere2)
-        }
-
-
-
         const sphereGeometry = new THREE.SphereGeometry(0.3,32,16)
-        const sphereMaterial = new THREE.MeshMatcapMaterial({map: texture})
+        const sphereMaterial = new THREE.MeshMatcapMaterial({map: crystalPower1})
         const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
         sphere.rotation.y = 4.6
         sphere.position.x = -0.2
@@ -100,7 +83,7 @@ fontLoader.load(
                         bevelSegments: 5
                     }
                 )
-                const textMaterial1 = new THREE.MeshBasicMaterial({color: 'yellow'})
+                const textMaterial1 = new THREE.MeshBasicMaterial({map: yellowFont})
                 textGeometry1.center()
                 const text1 = new THREE.Mesh(textGeometry1, textMaterial1)
                 
@@ -124,7 +107,7 @@ fontLoader.load(
                         bevelSegments: 5
                     }
                 )
-                const textMaterial2 = new THREE.MeshBasicMaterial({color: 'red'})
+                const textMaterial2 = new THREE.MeshBasicMaterial({map:redFont})
                 textGeometry2.center()
                 const text2 = new THREE.Mesh(textGeometry2, textMaterial2)
                 
@@ -136,16 +119,62 @@ fontLoader.load(
     }
 )
 
-/**
- * Object
- */
+const sphereGeometry2 = new THREE.SphereGeometry(0.3,32,16)
+const sphereMaterial2 = new THREE.MeshMatcapMaterial({map: crystalPower2})
+const sphere2 = new THREE.Mesh(sphereGeometry2, sphereMaterial2)
+scene.add(sphere2)
+sphere2.position.x = 0.5
+sphere2.position.y = 1.5
+sphere2.position.z = -1.5
+sphere2.rotation.y = 4.6
+
+const sphereGeometry3 = new THREE.SphereGeometry(0.3,32,16)
+const sphereMaterial3 = new THREE.MeshMatcapMaterial({map: crystalPower3})
+const sphere3 = new THREE.Mesh(sphereGeometry3, sphereMaterial3)
+scene.add(sphere3)
+sphere3.position.x = -2
+sphere3.position.y = -1
+sphere3.rotation.y = 4.6
+
+const sphereGeometry4 = new THREE.SphereGeometry(0.3,32,16)
+const sphereMaterial4 = new THREE.MeshMatcapMaterial({map: crystalPower4})
+const sphere4 = new THREE.Mesh(sphereGeometry4, sphereMaterial4)
+scene.add(sphere4)
+sphere4.position.x = -1.5
+sphere4.position.y = 1.7
+sphere4.rotation.y = 4.6
+
+const sphereGeometry5 = new THREE.SphereGeometry(0.3,32,16)
+const sphereMaterial5 = new THREE.MeshMatcapMaterial({map: crystalPower5})
+const sphere5 = new THREE.Mesh(sphereGeometry5, sphereMaterial5)
+scene.add(sphere5)
+sphere5.position.y = -1.5
+sphere5.position.x = 2
+sphere5.rotation.y = 4.6
+
+const sphereGeometry6 = new THREE.SphereGeometry(0.3,32,16)
+const sphereMaterial6 = new THREE.MeshMatcapMaterial({map: crystalPower6})
+const sphere6 = new THREE.Mesh(sphereGeometry6, sphereMaterial6)
+scene.add(sphere6)
+sphere6.position.y = 1.4
+sphere6.position.x = 2
+sphere6.position.z = -1
+sphere6.rotation.y = 4.6
+
+const sphereGeometry7 = new THREE.SphereGeometry(0.3,32,16)
+const sphereMaterial7 = new THREE.MeshMatcapMaterial({map: crystalPower7})
+const sphere7 = new THREE.Mesh(sphereGeometry7, sphereMaterial7)
+scene.add(sphere7)
+sphere7.position.y = -1.6 
+sphere7.position.z = 1
+sphere7.rotation.y = 4.6
 
 
 
 
-/**
- * Sizes
- */
+
+
+
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -166,10 +195,8 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-/**
- * Camera
- */
-// Base camera
+
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 // gui.add(camera.position, 'x')
 // gui.add(camera.position, 'y')
@@ -179,37 +206,32 @@ camera.position.y = 0
 camera.position.z = 5
 scene.add(camera)
 
-// Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-/**
- * Renderer
- */
+
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-/**
- * Animate
- */
+
 const clock = new THREE.Clock()
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
-    // sphere.rotation.y += 0.01
+    
 
-    // Update controls
+    
     controls.update()
 
-    // Render
+    
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame
+    
     window.requestAnimationFrame(tick)
 }
 
